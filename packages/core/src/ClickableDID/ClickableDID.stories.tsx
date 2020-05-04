ef1886ec6def0b10b5a7e06affe14aa935febfdb
Orie Steele <orie@transmute.industries>
2020-05-04 17:00:54 -0500

    fix test

--- packages/core/src/ClickableDID/ClickableDID.stories.tsx
+++ packages/core/src/ClickableDID/ClickableDID.stories.tsx
@@ -5,39 +5,15 @@ export default {
   title: 'Clickable DID',
 };
 
+const did = 'did:elem:ropsten:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg';
 const handleClick = () => {
   console.log('clicked');
 };
 
-const cases = [
-  {
-    did: 'did:example:123',
-    onClick: handleClick,
-  },
-  {
-    did: 'did:key:z6Mkh2o8dSUqfZMr351aMsPNyU4X4nhZybZBNsFqjKLi4BUQ',
-    onClick: handleClick,
-  },
-  {
-    did: 'did:elem:ropsten:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg',
-    onClick: handleClick,
-  },
-  {
-    did: 'did:web:did.actor:alice',
-    onClick: handleClick,
-  },
-];
-
 // By passing optional props to this story, you can control the props of the component when
 // you consume the story in a test.
-export const Examples = (props?: Partial<IClickableDIDProps>) => (
+export const Element = (props?: Partial<IClickableDIDProps>) => (
   <div>
-    {cases.map((c: any) => {
-      return (
-        <div key={c.did} style={{ padding: '2px' }}>
-          <ClickableDID {...c} />
-        </div>
-      );
-    })}
+    <ClickableDID did={did} onClick={handleClick} {...props} />
   </div>
 );
--- packages/core/src/ClickableDID/ClickableDID.test.tsx
+++ packages/core/src/ClickableDID/ClickableDID.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Examples as ClickableDIDStories } from './ClickableDID.stories';
+import { Element } from './ClickableDID.stories';
 
 describe('ClickableDID', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<ClickableDIDStories />, div);
+    ReactDOM.render(<Element />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
