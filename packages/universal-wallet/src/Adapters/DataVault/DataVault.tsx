38de123987231290cdb4b8fced1b4df98a43af4f
gjgd <gjgd@protonmail.com>
2020-07-14 15:32:23 +0000

    chore: cleanup

--- packages/universal-wallet/src/Adapters/DataVault/DataVault.tsx
+++ packages/universal-wallet/src/Adapters/DataVault/DataVault.tsx
@@ -1,19 +1,13 @@
 import React, { FC, HTMLAttributes } from 'react';
-
 import { makeStyles } from '@material-ui/core/styles';
-
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-
 import Avatar from '@material-ui/core/Avatar';
-
 import Typography from '@material-ui/core/Typography';
-
 import Button from '@material-ui/core/Button';
-
 import EnhancedEncryptionIcon from '@material-ui/icons/EnhancedEncryption';
 import SyncIcon from '@material-ui/icons/Sync';
 
--- packages/universal-wallet/src/UniversalWallet.tsx
+++ packages/universal-wallet/src/UniversalWallet.tsx
@@ -1,14 +1,11 @@
 import React, { FC, HTMLAttributes } from 'react';
-
 import { makeStyles } from '@material-ui/core/styles';
-
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
